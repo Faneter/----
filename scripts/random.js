336d5478aa224ee76ad7e6223787c4dd3d5d6bb0
Faneter <Faneter@outlook.com>
2024-07-18 18:14:48 +0800

    优化随机数生成，使其不产生重复数字

--- scripts/random.js
+++ scripts/random.js
@@ -1,7 +1,9 @@
+// 此文件下所有的随机数范围都是双闭的
+
 function getRandomIntergers(from, to, num) {
     var array = new Array(num)
     for (i = 0; i < num ;i++) {
-        array[i] = Math.floor(Math.random()*(to - from)) + from
+        array[i] = Math.floor(Math.random()*(to - from + 1)) + from
     }
 
     return array
@@ -9,17 +11,15 @@ function getRandomIntergers(from, to, num) {
 
 function getRandomNonrepetitiveIntergers(from, to, num) {
     var array = new Array(num)
-    for (i = 0; i < num ;i++) {
-        var j = Math.floor(Math.random()*(to - from)) + from
-    }
-
-    return array
-}
-
-function getRandomIntergers(from, to, num) {
-    var array = new Array(num)
-    for (i = 0; i < num ;i++) {
-        array[i] = Math.floor(Math.random()*(to - from)) + from
+    F: for (i = 0; i < num ;i++) {
+        var j = Math.floor(Math.random()*(to - from + 1)) + from
+        for (k = 0; k < i; k++) {
+            if (array[k] == j) {
+                i--
+                continue F
+            }
+        }
+        array[i] = j
     }
 
     return array
@@ -27,12 +27,33 @@ function getRandomIntergers(from, to, num) {
 
 function getRandomIntergersWithoutSelectedNum(from, to, num, selectedNum) {
     var array = new Array(num)
-    for (i = 0; i < num ;) {
-        var n = Math.floor(Math.random()*(to - from)) + from
-        if (n != selectedNum) {
-            array[i] = n
-            i++
+    for (i = 0; i < num ;i++) {
+        var j = Math.floor(Math.random()*(to - from + 1)) + from
+        if (j == selectedNum) {
+            i--
+            continue
         }
+        array[i] = j
+    }
+
+    return array
+}
+
+function getRandomNonrepetitiveIntergersWithoutSelectedNum(from, to, num, selectedNum) {
+    var array = new Array(num)
+    F: for (i = 0; i < num ;i++) {
+        var j = Math.floor(Math.random()*(to - from + 1)) + from
+        if (j == selectedNum) {
+            i--
+            continue
+        }
+        for (k = 0; k < i; k++) {
+            if (array[k] == j) {
+                i--
+                continue F
+            }
+        }
+        array[i] = j
     }
 
     return array
@@ -40,10 +61,26 @@ function getRandomIntergersWithoutSelectedNum(from, to, num, selectedNum) {
 
 function getRandomIntergersWithSelectedNum(from, to, num, selectedNum) {
     var array = new Array(num)
-    var selectedNumIndex = getRandomIntergers(0, num - 1, 1)
+    var selectedNumIndex = getRandomIntergers(0, num - 1, 1)[0]
     array[selectedNumIndex] = selectedNum
 
     var nums = getRandomIntergersWithoutSelectedNum(from, to, num, selectedNum);
+
+    for(i = 0; i < num; i++) {
+        if (i != selectedNumIndex) {
+            array[i] = nums[i]
+        }
+    }
+
+    return array
+}
+
+function getRandomNonrepetitiveIntergersWithSelectedNum(from, to, num, selectedNum) {
+    var array = new Array(num)
+    var selectedNumIndex = getRandomIntergers(0, num - 1, 1)[0]
+    array[selectedNumIndex] = selectedNum
+
+    var nums = getRandomNonrepetitiveIntergersWithoutSelectedNum(from, to, num, selectedNum);
 
     for(i = 0; i < num; i++) {
         if (i != selectedNumIndex) {
